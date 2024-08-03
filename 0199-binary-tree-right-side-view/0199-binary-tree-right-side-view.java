4eb8a406536f04f0d483aedef65f04ae8d593cfb
bhavatharany03 <166037679+bhavatharany03@users.noreply.github.com>
2024-08-03 23:13:30 +0530

    Time: 1 ms (66.94%), Space: 42 MB (39.41%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+       List<Integer> rightView = new ArrayList<>();
+        if (root == null) {
+            return rightView;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // If it's the last node in the current level, add it to the result
+                if (i == levelSize - 1) {
+                    rightView.add(currentNode.val);
+                }
+                // Add left and right children to the queue
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+        }
+
+        return rightView;
+    }
+
+}
