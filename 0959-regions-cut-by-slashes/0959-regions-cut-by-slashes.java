11f6a4e87ccb4687e07aabed46a7e4c6a4203831
bhavatharany03 <166037679+bhavatharany03@users.noreply.github.com>
2024-08-10 19:09:17 +0530

    Time: 6 ms (58.88%), Space: 42.1 MB (49.24%) - LeetHub

---
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.java
@@ -0,0 +1,78 @@
+class Solution {
+    class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // Path compression
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+
+    public int regionsBySlashes(String[] grid) {
+         int n = grid.length;
+        UnionFind uf = new UnionFind(4 * n * n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int index = 4 * (i * n + j);
+                char c = grid[i].charAt(j);
+
+                if (c == '/') {
+                    uf.union(index, index + 3);
+                    uf.union(index + 1, index + 2);
+                } else if (c == '\\') {
+                    uf.union(index, index + 1);
+                    uf.union(index + 2, index + 3);
+                } else {
+                    uf.union(index, index + 1);
+                    uf.union(index + 1, index + 2);
+                    uf.union(index + 2, index + 3);
+                }
+
+                if (j < n - 1) {
+                    uf.union(index + 1, index + 7);
+                }
+
+                if (i < n - 1) {
+                    uf.union(index + 2, index + 4 * n);
+                }
+            }
+        }
+
+        int regions = 0;
+        for (int i = 0; i < 4 * n * n; i++) {
+            if (uf.find(i) == i) {
+                regions++;
+            }
+        }
+        return regions;
+    }
+}
+        
+    
